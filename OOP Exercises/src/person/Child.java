7a885186bfbff324c7acbd1504ad09e1f72a6f08
Georgi Georgiev <63241083+georgi9820@users.noreply.github.com>
2024-07-28 11:01:04 +0300

    new task Person

---
+++ OOP Exercises/src/person/Child.java
@@ -0,0 +1,11 @@
+package person;
+
+public class Child extends Person{
+    private String name;
+    private int age;
+
+
+    public Child(String name, int age) {
+        super(name, age);
+    }
+}
